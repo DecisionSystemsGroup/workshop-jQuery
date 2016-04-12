25aa7da29dfa56310f836b8107dfce5a7797b842
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-04-12 23:15:48 +0300

    Εισαγωγή event listener/handler για τα φίλτρα

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 ﻿$(document).ready(function(){
-	
+	$(".menu-button").on("click", function (){
+		$(".data-table tbody tr").hide();	//Kripsimo olwn twn grammwn
+		
+		var filter = $(this).data("filter");	//Apothikeush ths timhs tou filtrou pou patithike
+		
+		if(filter!="all"){	//Se periptosh pou theloume na ta emfanisoume ola den xriazete na prosthesoume to filtro
+			var selector = ".data-table tr."+"course-"+filter;
+		}
+		else{
+			var selector = ".data-table tr";
+		}
+		
+		$(selector).fadeIn();	//Emfanish twn grammwn pou antistixoun sto filtro mas
+	});
 });
