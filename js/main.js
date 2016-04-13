2710d728cd8b91a14245648e8081de2b2581d18f
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-04-13 11:22:33 +0300

    Υλοποίηση κουμπιών "Περιγραφή"

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function(){
+$(document).ready(function(){
 	$(".menu-button").on("click", function (){
 		$(".data-table tbody tr").hide();	//Kripsimo olwn twn grammwn
 		
@@ -13,7 +13,57 @@
 		
 		$(selector).fadeIn();	//Emfanish twn grammwn pou antistixoun sto filtro mas
 		
-		var title =  $(this).text();
-		$("#panel-title").text(title);	//Allagh tou title
+		var title = $(this).text();
+		$("#panel-title").text(title);
+	});
+	
+	
+	$(".button-more").on("click", function(){
+		$(".panel").hide();
+		$("#info").show();
+		
+		var courseId = $(this).closest("tr").data("course-id");	//Euresh tou id tou mathimatos
+		
+		//Perasma periexomenou sto kathe pedio
+		$("#panel-title").text(info[courseId]['name']);	
+		$("#info-semester").text(info[courseId]['semester']);
+		$("#info-description").text(info[courseId]['description']);
+		$("#info-complete").text(info[courseId]['complete']);
+		
+		//Prosthikh katalilou class gia to an to mathima einai oloklhromeno
+		if( info[courseId]['complete']=="Ναί" ){
+			$("#info-complete").addClass("success").removeClass("fail");
+		}
+		else{
+			$("#info-complete").addClass("fail").removeClass("success");
+		}
 	});
 });
+
+//dummy data
+var info =	{
+		0: {
+			"semester": "1o",
+			"name": "Προγραμματισμός 1",
+			"description": "Εξοικείωση του φοιτητή με βασικές έννοιες του προγραμματισμού.",
+			"complete": "Ναί",
+		},
+		1: {
+			"semester": "2o",
+			"name": "Δομές Δεδομένων",
+			"description": "Ταξινόμηση και αναζήτηση πίνακα, ουρές, στοίβες, λίστες, πίνακες κατακερματισμού, δυαδικά δέντρα αναζήτησης, ισοζυγισμένα δέντρα αναζήτησης, σωροί, ταξινόμηση σωρού, αναπαράσταση γράφων, διάτρεξη γράφων.",
+			"complete": "Όχι",
+		},
+		2: {
+			"semester": "3o",
+			"name": "Λειτουργικά συστήματα",
+			"description": "Το μάθημα αποσκοπεί, αρχικά, στην παρουσίαση των βασικών στοιχείων των Λειτουργικών Συστημάτων, στην ανάδειξη των διαφορών τους και στην παρουσίαση των παιτουμένων υπηρεσιών των χρηστών από τα σύγχρονα Λειτουργικά Συστήματα.",
+			"complete": "Όχι",
+		},
+		3: {
+			"semester": "5o",
+			"name": "Τεχνολογία Λογισμικού",
+			"description": "Εξοικείωση ακόμη καλύτερα με την ανάπτυξη λογισμικού μέσω της χρήσης γλωσσών αντικειμενοστραφούς σχεδίασης συστημάτων, όπως η UML.",
+			"complete": "Ναί",
+		}
+}
